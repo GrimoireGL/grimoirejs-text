798d65102d1694a7d4865ab8031875494042175b
case2912 <shiroyama@maia.eonet.ne.jp>
2017-02-13 12:57:19 +0900

    Start branch for fix-draw-method

--- src/TextComponent.ts
+++ src/TextComponent.ts
@@ -13,17 +13,9 @@ export default class TextComponent extends Component {
             default: "Hello Grimoire!",
             converter: "String"
         },
-        textureSize: {
-            default: 256,
-            converter: "Number"
-        },
         font: {
-            default: "Arial",
+            default: "",
             converter: "String"
-        },
-        fontSize: {
-            default: 30,
-            converter: "Number"
         },
         stroke: {
             default: false,
@@ -33,12 +25,11 @@ export default class TextComponent extends Component {
     private text: string;
     private lastText: string;
     private font: string;
-    private fontSize: number;
     private stroke: boolean;
     private textureSize: number;
     private transform: TransformComponent;
     private scale: Vector3;
-    public $awake(): void {
+    public $mount(): void {
         this.__bindAttributes();
         this.draw(this.text);
         this.lastText = this.text;
@@ -49,25 +40,14 @@ export default class TextComponent extends Component {
             this.lastText = this.text;
         }
     }
-    private normalize(x: number, y: number, scale: Vector3): string {
-        const r = Math.pow(10, Math.max(String(x).length, String(y).length) - 1);
-        return x / r * scale.X + "," + y / r * scale.Y;
-    }
-    private draw(text: string) {
-        this.transform = this.node.getComponent("Transform") as TransformComponent;
-        this.scale = this.transform.localScale;
-        const canvas = document.createElement("canvas");
+    private measure(font: string): number[] {
+        const canvas = document.createElement('canvas');
         var ctx = canvas.getContext('2d');
-        // const body = document.getElementsByTagName("body")[0];
-        // body.appendChild(canvas);
-        canvas.width = this.textureSize;
-        canvas.height = this.textureSize;
         ctx.textBaseline = 'top';
-        ctx.font = this.fontSize + "px " + this.font;
-        ctx.fillStyle = 'rgb(255, 255, 255)';
-        ctx.strokeStyle = 'rgb(255, 255, 255)';
-
-        ctx.fillText(text, 0, 0);
+        ctx.font = font;
+        canvas.width = 256;
+        canvas.height = 256;
+        ctx.fillText("a", 0, 0);
         var pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
         var data = pixels.data;
         var textHeight = 0;
@@ -101,28 +81,30 @@ export default class TextComponent extends Component {
         }
         textHeight = textHeight - firstRow;
         textWidth = textWidth - firstCulumn;
-        const canvas2 = document.createElement("canvas");
-        canvas2.setAttribute("width", String(textWidth));
-        canvas2.setAttribute("height", String(textHeight));
-        var ctx2 = canvas2.getContext('2d');
-        ctx2.drawImage(canvas, firstCulumn, firstRow, canvas2.width, canvas2.height, 0, 0, canvas2.width, canvas2.height);
-        const texture = canvas2.toDataURL();
 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.scale(canvas.width / textWidth, canvas.height / textHeight);
-
-        if (this.stroke == true) {
-            ctx.strokeText(text, -firstCulumn, -firstRow);
-        } else {
-            ctx.fillText(text, -firstCulumn, -firstRow);
+        return [textWidth, textHeight];
+    }
+    private draw(text: string) {
+        this.transform = this.node.getComponent("Transform") as TransformComponent;
+        this.scale = this.transform.localScale;
+        const canvas = document.createElement("canvas");
+        var ctx = canvas.getContext('2d');
+        canvas.width = this.textureSize;
+        canvas.height = this.textureSize;
+        ctx.textBaseline = 'top';
+        ctx.font = this.font;
+        console.log()
+        // ctx.fillStyle = 'rgb(255, 255, 255)';
+        // ctx.strokeStyle = 'rgb(255, 255, 255)';
+        const length = this.measure(this.font);
+        const textLength = this.text.length;
+        const d = Math.ceil(Math.pow(textLength, 0.5));
+        canvas.width = Math.max(length[0], length[1]) * d;
+        canvas.height = canvas.width;
+        const body = document.getElementsByTagName("body")[0];
+        body.appendChild(canvas);
+        for (let i = 0; i < textLength; i+=d) {
+            ctx.fillText(this.text, length[1] * i, 0);
         }
-        //
-        // ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
-        // canvas2.width = canvas.width;
-        // canvas2.height = canvas.height;
-        // ctx2.transform(-1, 0, 0, 1, canvas.width, 0);
-        // ctx2.drawImage(canvas, 0, 0);
-        this.node.setAttribute("texture", canvas, false);
-        this.node.setAttribute("scale", this.normalize(textWidth, textHeight, this.scale) + ",0");
     }
 }
