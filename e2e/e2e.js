a0470d508bfa13a162f200bffb5c857a72966d88
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-03-25 18:15:10 +0900

    chore: enlonged timeout length

--- e2e/e2e.js
+++ e2e/e2e.js
@@ -14,7 +14,7 @@ for(let key of dirs){
   exportModule[key] = (client)=>{
     client
         .url(`http://localhost:8080/e2e/${key}/index.html`)
-        .waitForElementPresent(`canvas.gr-resource-loaded-canvas`,5000)
+        .waitForElementPresent(`canvas.gr-resource-loaded-canvas`,30000)
         .pause(100)
         .saveScreenshot(dir + `/${key}.png`).end();
   };
